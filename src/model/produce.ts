e9a13ff6d7b614a08eed43b3b10631a9e6f3c7bf
Olivia Powell <olive.p@outlook.com>
2018-10-13 17:25:54 -0400

    new list with produce

---
+++ src/model/produce.ts
@@ -0,0 +1,3 @@
+export class Produce {
+    constructor(public name, public price) {}
+}
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Produce } from '../../model/produce';
 
 @Component({
   selector: 'page-list',
@@ -7,25 +8,21 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ListPage {
   selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  private items = [
+    new Produce('Gala Apple', 1.10),
+    new Produce('Zucchini', 1.10),
+    new Produce('Pear', 1.10),
+    new Produce('Cucumber', 1.10),
+    new Produce('Butternut Squash', 1.10),
+    new Produce('Pumkin', 1.10),
+    new Produce('Peach', 1.10),
+    new Produce('Orange', 1.10)
+  ];
+  private allItems = this.items;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
   }
 
   itemTapped(event, item) {
@@ -33,5 +30,23 @@ export class ListPage {
     this.navCtrl.push(ListPage, {
       item: item
     });
+  }
+
+  private searchItems(searchbar) {
+    this.items = this.allItems
+    var query = searchbar.srcElement.value;
+    if(!query) {return;}
+    this.items = this.items.filter((item) => {
+    if(item.name && query) {
+      if (item.name.toLowerCase().indexOf(query.toLowerCase()) > -1) {
+        return true;
+      }
+      return false;
+    }
+  });
+  }
+
+  private cancelSearch(searchbar) {
+    this.items = this.allItems;
   }
 }
